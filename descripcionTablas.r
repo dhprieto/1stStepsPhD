b8df5367276b3d2210c259b984db802d91e6bcb4
mozoezomoe <34859759+mozoezomoe@users.noreply.github.com>
2021-06-15 12:04:40 +0200

    Add files via upload

---
+++ descripcionTablas.r
@@ -0,0 +1,17 @@
+#----  Descripción datos
+
+## Compuestos fenólicos
+
+
+
+### Compuestos fenolicos SA VidaUtil Cronico
+
+### Compuestos fenolicos ST VidaUtil Cronico
+
+### Compuestos fenolicos SU VidaUtil Cronico
+
+## Muestras bio
+
+### Cronico Orina Atipicos
+
+### Cronico plasma Atipicos
