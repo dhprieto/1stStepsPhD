c39a9f80475eac19ac3b5abd857071e8f5867542
mozoezomoe <dres2112@gmail.com>
2021-09-08 12:52:48 +0200

    merged and saved!

--- preprocess.R
+++ preprocess.R
@@ -21,7 +21,7 @@ preprocess <- function(tablePath, nasPercentageCol, nasRow){
   if (grepl("agudo", tablePath)){agudo = T}
   else {agudo = F}
   
-  if(agudo == T){
+  if(agudo){
     
     for (i in seq(1, nrow(table))){
       if (grepl(pattern = "([A-C]) -1", x = table$X[i])){
@@ -95,8 +95,7 @@ preprocess <- function(tablePath, nasPercentageCol, nasRow){
 
   
   
-
-tableName2 = "cronicoOrinaANtLimpio.csv"
+tableName2 = "cronicoPlasmaAntLimpio.csv"
 
 rootDir = "data/"
 
@@ -104,63 +103,7 @@ tablePath = paste0(rootDir,tableName2)
 
 cronicoOrinaAnt_Antro <- preprocess (tablePath, 0.05, T)
 
+saveRDS(cronicoOrinaAnt_Antro, "data/cronicoPlasmaAnt_Antro.csv")
 
 
 
-View(agudoOrinaAnt)
-
-
-tablaa <- read.csv("data/datosAntropometricosCardiovasculares.csv", sep =";", dec=",")
-
-str(tablaa)
-
-for (i in seq(1, nrow(tablaa))){
-  tablaa$numVol[i] <- gsub("[A-C].", "", tablaa[,1][i])}
-
-
-
-round(6.9)
-round(rescale(antro_ST[,1], to=c(min(as.integer(agudoOrinaAnt[,1])),max(as.integer(agudoOrinaAnt[,1])))))
-
-
-# procesado tabla de datosantro
-  
-
-antro <- read.csv("data/datosAntropometricosCardiovasculares.csv", sep = ";", dec = ",")
-
-for (i in seq(1,nrow(antro))){
-  if (antro$Nº.Volunt.[i] <= 46){
-    antro$Endulzante[i] <- "ST"
-    
-  }
-  if ((antro$Nº.Volunt.[i] >= 51) & (antro$Nº.Volunt.[i] <= 96)){
-    antro$Endulzante[i] <- "SU"
-    
-  }
-  if ((antro$Nº.Volunt.[i] >= 101) & (antro$Nº.Volunt.[i] <= 146)){
-    antro$Endulzante[i] <- "SA"
-    
-  }  
-}
-
-
-## funcion rescale funciona
-
-x<-seq(1,20)
-y <- c(30,31,34,35,37,38,40,42,43,45,47,49,50)
-library(scales)
-round(rescale (y, to=c(1,20), from=c(30,50)))
-
-checkReesc <- function(tabla, nColumna){
-  for (i in seq(2, nrow(tabla))){
-    if (tabla[,nColumna][i-1] == tabla[,nColumna][i]){
-      tabla[,nColumna][i] = tabla[,nColumna][i]+1
-    }
-  }
-}
-
-checkReesc(agudoOrinaAnt,21)
-
-library(dplyr)
-
-filter(agudoOrinaFlav, (agudoOrinaFlav$Endulzante == "ST") & (agudoOrinaFlav$Tiempo == "-1"))
