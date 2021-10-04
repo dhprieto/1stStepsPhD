b5b0d7fdfefa6b4d5e831eefda43ddaf033e69aa
mozoezomoe <dres2112@gmail.com>
2021-10-04 13:33:57 +0200

    back from accident :)

--- Clustering.R
+++ Clustering.R
@@ -25,7 +25,98 @@ set.A_num <- set.A[,-c(1,7,26)]
 set.A_rescaled <- set.A_num %>% mutate_each_(list(~rescale(.) %>% as.vector), 
                                          vars = colnames(set.A_num))
 
+# Validación de método de clustering ----
 
+## Manual ----
+
+library(purrr)
+library(ggpubr)
+datos_simulados <- map_df(set.A_rescaled,
+                          .f = function(x){runif(n = length(x),
+                                                 min = min(x),
+                                                 max = max(x))
+                          }
+)
+
+
+pca_datos_A      <- prcomp(set.A_rescaled)
+pca_datos_simulados <- prcomp(datos_simulados)
+p1 <- fviz_pca_ind(X = pca_datos_A, habillage = set.A$Sexo,
+                   geom = "point", title = "PCA - datos uwu",
+                   pallete = "jco") +
+  theme_bw() + theme(legend.position = "bottom")
+p2 <- fviz_pca_ind(X = pca_datos_simulados, geom = "point",
+                   title = "PCA - datos simulados", pallete = "jco") +
+  theme_bw() + theme(legend.position = "bottom")
+
+ggarrange(p1, p2)#, common.legend = TRUE)
+
+### Aplicamos clustering ----
+
+# K-means clustering
+
+km_datos_A <- kmeans(x = set.A_rescaled, centers = 2)
+p1 <- fviz_cluster(object = km_datos_A, data = set.A_rescaled,
+                   ellipse.type = "norm", geom = "point", main = "Datos sexo",
+                   stand = FALSE, palette = "jco") +
+  theme_bw() + theme(legend.position = "none")
+km_datos_simulados <- kmeans(x = datos_simulados, centers = 2)
+p2 <- fviz_cluster(object = km_datos_simulados, data = datos_simulados,
+                   ellipse.type = "norm", geom = "point",
+                   main = "Datos simulados", stand = FALSE, palette = "jco") +
+  theme_bw() + theme(legend.position = "none")
+
+# Hierarchical clustering
+p3 <- fviz_dend(x = hclust(dist(set.A_rescaled)), k = 2, k_colors = "jco",
+                show_labels = FALSE, main = "Datos sexo")
+p4 <- fviz_dend(x = hclust(dist(datos_simulados)), k = 2, k_colors = "jco",
+                show_labels = FALSE, main = "Datos simulados")
+
+ggarrange(p1, p2)
+
+ggarrange(p3, p4)
+
+### Estadístico de Hopkins ----
+
+library(clustertend)
+set.seed(101)
+
+hopkins(set.A_rescaled, n = nrow(set.A_rescaled)-1)
+hopkins(datos_simulados, n = nrow(datos_simulados)-1)
+
+### VAT (Comparación visual) ----
+
+dist_datos_A      <- dist(set.A_rescaled, method = "euclidean")
+dist_datos_simulados <- dist(datos_simulados, method = "euclidean")
+
+p1 <- fviz_dist(dist.obj = dist_datos_A, show_labels = FALSE) +
+  labs(title = "Datos sexo") + theme(legend.position = "bottom")
+p2 <- fviz_dist(dist.obj = dist_datos_simulados, show_labels = FALSE) +
+  labs(title = "Datos simulados") + theme(legend.position = "bottom")
+
+ggarrange(p1, p2)
+
+
+## Automática ----
+
+library(clValid)
+
+comparacion <- clValid(
+  obj        = set.A_rescaled,
+  nClust     = 2:6,
+  clMethods  = c("hierarchical", "kmeans", "pam"),
+  validation = c("stability", "internal")
+)
+summary(comparacion)
+
+# Número de clusters ----
+
+library(NbClust)
+
+numero_clusters <- NbClust(data = scale(set.A_num), distance = "euclidean", min.nc = 2,
+                           max.nc = 5, method = "kmeans", index = c("kl","ch","hartigan",  "cindex", "db"))
+
+fviz_nbclust(numero_clusters)
 
 # Non hierarchical clustering ----
 
@@ -85,7 +176,42 @@ cor(x = mat_dist, cophenetic(hc_euclidea_average))
 
 set.seed(101)
 
-fviz_dend(x = hc_euclidea_complete, k = 4, cex = 0.6) +
+fviz_dend(x = hc_euclidea_average, k = 2, cex = 0.6) +
   geom_hline(yintercept = 5.5, linetype = "dashed") +
   labs(title = "Herarchical clustering",
-       subtitle = "Distancia euclídea, Linkage complete, K=4")
+       subtitle = "Distancia euclídea, Linkage average, K=2")
+
+library("igraph")
+set.seed(101)
+fviz_dend(x = hc_euclidea_average,
+          k = 5,
+          k_colors = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"),
+          color_labels_by_k = TRUE,
+          cex = 0.8,
+          type = "phylogenic",
+          repel = TRUE)
+
+
+# Otros métodos ----
+## DBSCAN ----
+
+library(fpc)
+library(dbscan)
+library(Rcpp)
+
+dbscan::kNNdistplot(set.A_rescaled, k=5) # no acaba
+
+
+
+
+# HEATMAPS ----
+
+heatmap(x = as.matrix(set.A_rescaled), scale = "none",
+        distfun = function(x){dist(x, method = "euclidean")},
+        hclustfun = function(x){hclust(x, method = "average")},
+        cexRow = 0.7)
+
+library(pheatmap)
+pheatmap(mat = as.matrix(set.A_rescaled), scale = "none", clustering_distance_rows = "euclidean",
+         clustering_distance_cols = "euclidean", clustering_method = "average",
+         cutree_rows = 4, fontsize = 6)
