83fdeba43354f3181951512f849b79a2f5c7374b
mozoezomoe <dres2112@gmail.com>
2021-11-18 12:56:13 +0100

    pca-formatting ;)

--- article1.R
+++ article1.R
@@ -5,6 +5,7 @@ library(ggpubr)
 library(factoextra)
 library(GGally)
 library(plotly)
+library(FactoMineR)
 #library(clustertend)
 
 preprocessTablas <- function(root, nombreTabla) {
@@ -45,7 +46,7 @@ preprocessTablas <- function(root, nombreTabla) {
   set.A_factors <- cbind(set.A_rescaled, Endulzante = set.A$Endulzante, 
                          Tiempo = set.A$Tiempo, Sexo = set.A$Sexo)
   
-  return(list(tablaFactors = set.A, tablaNum = set.A_rescaled))
+  return(list(tablaFactors = set.A_factors, tablaNum = set.A_rescaled))
   
 }
 
@@ -115,13 +116,13 @@ clusterVarios <- function (tabla, nclust) {
   
   print(p2)
   
-  print(fviz_contrib(km_datos_A, choice="var", top = 13))
+  
   
 }
 
-pcaVarios<- function(tabla){
+pcaVarios<- function(tabla, nombreTabla){
   
-  nombreTabla <- deparse(quote(tabla))
+  #nombreTabla <- deparse(quote(tabla))
   
   tabla_Tiempo <- subset(tabla, select=-c(Endulzante, Sexo))
   
@@ -138,48 +139,214 @@ pcaVarios<- function(tabla){
   tabla_EndulzanteSA <- subset(tabla_Endulzante, Endulzante == "SA", select = -Endulzante)
   tabla_EndulzanteSU <- subset(tabla_Endulzante, Endulzante == "SU", select = -Endulzante)
   tabla_EndulzanteST <- subset(tabla_Endulzante, Endulzante == "ST", select = -Endulzante)
-  
-  return (list(
-    tabla_Tiempo,
-    tabla_Tiempo0,
-    tabla_TiempoF,
-    tabla_Sexo, 
-    tabla_SexoM,
-    tabla_SexoF,
-    tabla_Endulzante,
-    tabla_EndulzanteSA,
-    tabla_EndulzanteSU,
-    tabla_EndulzanteST
-  ))
 
   
   # PCA Tiempo
   
-  pca_datos_A      <- prcomp(tabla_Tiempo0)
-  print(fviz_contrib(pca_datos_A, choice="var", top = 13, title = paste("Importancia
-                     variables PCA Tiempo 0 ", nombreTabla)))
-  pca_datos_B      <- prcomp(tabla_TiempoF)
-  print(fviz_contrib(pca_datos_B, choice="var", top = 13, title = paste("Importancia
-                     variables PCA Tiempo Final ", nombreTabla)))
+  pca_datos_A <- PCA(tabla_Tiempo0, scale.unit = F)
+  print(fviz_contrib(pca_datos_A, choice="var", title = paste("Importancia variables dim 1 PCA Tiempo 0 ", 
+                                                                        nombreTabla)))
   
+  print(fviz_contrib(pca_datos_A, choice="var", axes = 2, title = paste("Importancia variables dim 2 PCA Tiempo 0 ", 
+                                                                        nombreTabla)))
+  pca_datos_B <- PCA(tabla_TiempoF)
+  
+  print(fviz_contrib(pca_datos_B, choice="var", axes=1, title = paste("Importancia variables dim 1 PCA Tiempo Final "
+                                                                        ,nombreTabla)))
+  print(fviz_contrib(pca_datos_B, choice="var", axes=2, title = paste("Importancia variables dim 2 PCA Tiempo Final "
+                                                                      ,nombreTabla)))
   # PCA Sexo
   
-  pca_datos_C      <- prcomp(tabla_SexoM)
-  print(fviz_contrib(pca_datos_C, choice="var", top = 13, title = paste("Importancia
-                     variables PCA Sexo M ", nombreTabla)))
-  pca_datos_D      <- prcomp(tabla_SexoF)
-  print(fviz_contrib(pca_datos_D, choice="var", top = 13, title = paste("Importancia
-                     variables PCA Sexo F ", nombreTabla)))
+  pca_datos_C <- PCA(tabla_SexoM,scale.unit = F)
+  print(fviz_contrib(pca_datos_C, choice="var", title = paste("Importancia variables dim 1 PCA Sexo M ", 
+                                                              nombreTabla)))
   
-  # PCA Sexo
+  print(fviz_contrib(pca_datos_C, choice="var", axes = 2, title = paste("Importancia variables dim 2 PCA Sexo M ", 
+                                                                        nombreTabla)))
+  pca_datos_D <- PCA(tabla_SexoF,scale.unit = F)
+  print(fviz_contrib(pca_datos_D, choice="var", title = paste("Importancia variables dim 1 PCA Sexo F ", 
+                                                              nombreTabla)))
   
-  pca_datos_E      <- prcomp(tabla_EndulzanteSA)
-  print(fviz_contrib(pca_datos_E, choice="var", top = 13, title = paste("Importancia variables PCA Endulzante SA ", nombreTabla)))
-  pca_datos_F      <- prcomp(tabla_EndulzanteSU)
-  print(fviz_contrib(pca_datos_F, choice="var", top = 13, title = paste("Importancia
-                                                                        variables PCA Endulzante SU ", nombreTabla)))
-  pca_datos_G      <- prcomp(tabla_EndulzanteST)
-  print(fviz_contrib(pca_datos_G, choice="var", top = 13))
+  print(fviz_contrib(pca_datos_D, choice="var", axes = 2, title = paste("Importancia variables dim 2 PCA Sexo F", 
+                                                                        nombreTabla)))
+  
+  
+  # PCA Endulzantes
+  
+  pca_datos_E      <- PCA(tabla_EndulzanteSA, scale.unit = F)
+  
+  print(fviz_contrib(pca_datos_E, choice="var", title = paste("Importancia variables dim 1 PCA Endulzante SA ", 
+                                                              nombreTabla)))
+  
+  print(fviz_contrib(pca_datos_E, choice="var", axes = 2, title = paste("Importancia variables dim 2 PCA Endulzante SA ", 
+                                                                        nombreTabla)))
+  
+  pca_datos_F      <- PCA(tabla_EndulzanteSU, scale.unit = F)
+  
+  print(fviz_contrib(pca_datos_F, choice="var", title = paste("Importancia variables dim 1 PCA Endulzante SU ", 
+                                                              nombreTabla)))
+  
+  print(fviz_contrib(pca_datos_F, choice="var", axes = 2, title = paste("Importancia variables dim 2 PCA Endulzante SU ", 
+                                                                        nombreTabla)))
+  
+  pca_datos_G      <- PCA(tabla_EndulzanteST, scale.unit = F)
+  print(fviz_contrib(pca_datos_G, choice="var", title = paste("Importancia variables dim 1 PCA Endulzante ST ", 
+                                                              nombreTabla)))
+  
+  print(fviz_contrib(pca_datos_G, choice="var", axes = 2, title = paste("Importancia variables dim 2 PCA Endulzante ST ", 
+                                                                        nombreTabla)))
+  # PCA TiempoxSexo
+  
+  tabla_0xM <- subset(tabla, Tiempo == "0" & Sexo == "HOMBRE", select =-c(Tiempo, Sexo, Endulzante))
+  tabla_0xF <- subset(tabla, Tiempo == "0" & Sexo == "MUJER", select =-c(Tiempo, Sexo, Endulzante))
+  tabla_FxM <- subset(tabla, Tiempo == "Final" & Sexo == "HOMBRE", select =-c(Tiempo, Sexo, Endulzante))
+  tabla_FxF <- subset(tabla, Tiempo == "Final" & Sexo == "MUJER", select =-c(Tiempo, Sexo, Endulzante))
+  
+  pca_0xM <- PCA(tabla_0xM, scale.unit=F)
+  print(fviz_contrib(pca_0xM, choice="var", title = paste("ContribVar dim1 Tiempo 0 Sexo M", nombreTabla)))
+  
+  print(fviz_contrib(pca_0xM, choice="var", axes = 2, title = paste("ContribVar dim2 Tiempo 0 Sexo M", 
+                                                                        nombreTabla)))
+  pca_0xF <- PCA(tabla_0xF, scale.unit=F)
+  
+  print(fviz_contrib(pca_0xF, choice="var", title = paste("ContribVar dim1 Tiempo 0 Sexo F", nombreTabla)))
+  
+  print(fviz_contrib(pca_0xF, choice="var", axes = 2, title = paste("ContribVar dim2 Tiempo 0 Sexo F", 
+                                                                    nombreTabla)))
+  
+  pca_FxM <- PCA(tabla_FxM, scale.unit=F)
+  
+  print(fviz_contrib(pca_FxM, choice="var", title = paste("ContribVar dim1 Tiempo F Sexo M", nombreTabla)))
+  
+  print(fviz_contrib(pca_FxM, choice="var", axes = 2, title = paste("ContribVar dim2 Tiempo F Sexo M", 
+                                                                    nombreTabla)))
+  pca_FxF <- PCA(tabla_FxF, scale.unit=F)
+  
+  print(fviz_contrib(pca_FxF, choice="var", title = paste("ContribVar dim1 Tiempo F Sexo F", nombreTabla)))
+  
+  print(fviz_contrib(pca_FxF, choice="var", axes = 2, title = paste("ContribVar dim2 Tiempo F Sexo F", 
+                                                                    nombreTabla)))
+  print(fviz_contrib(pca_0xM, choice="var", axes = 2, title = paste("ContribVar dim2 Tiempo 0 Sexo M", 
+                                                                    nombreTabla)))
+  
+  # PCA TiempoxEndulzante
+  
+  # PCA SexoxEndulzante
+  
+  tabla_MxSAx0 <- subset(tabla, Sexo == "HOMBRE" & Endulzante =="SA" & Tiempo == "0", 
+                         select =-c(Tiempo, Sexo, Endulzante))
+  
+  tabla_MxSAxF <- subset(tabla, Sexo == "HOMBRE" & Endulzante =="SA" & Tiempo == "Final", 
+                         select =-c(Tiempo, Sexo, Endulzante))
+  
+  tabla_FxSAx0 <- subset(tabla, Sexo == "MUJER" & Endulzante =="SA"& Tiempo == "0", 
+                         select =-c(Tiempo, Sexo, Endulzante))
+  tabla_FxSAxF <- subset(tabla, Sexo == "MUJER" & Endulzante =="SA"& Tiempo == "Final", 
+                         select =-c(Tiempo, Sexo, Endulzante))
+  
+  tabla_MxSTx0 <- subset(tabla, Sexo == "HOMBRE" & Endulzante =="ST" & Tiempo == "0", 
+                         select =-c(Tiempo, Sexo, Endulzante))
+  tabla_MxSTxF <- subset(tabla, Sexo == "HOMBRE" & Endulzante =="ST"& Tiempo == "Final", 
+                         select =-c(Tiempo, Sexo, Endulzante))
+  
+  tabla_FxSTx0 <- subset(tabla, Sexo == "MUJER" & Endulzante =="ST" & Tiempo == "0",
+                         select =-c(Tiempo, Sexo, Endulzante))
+  tabla_FxSTxF <- subset(tabla, Sexo == "MUJER" & Endulzante =="ST" & Tiempo == "Final",
+                         select =-c(Tiempo, Sexo, Endulzante))
+  
+  tabla_MxSUx0 <- subset(tabla, Sexo == "HOMBRE" & Endulzante =="SU" & Tiempo == "0", 
+                         select =-c(Tiempo, Sexo, Endulzante))
+  tabla_MxSUxF <- subset(tabla, Sexo == "HOMBRE" & Endulzante =="SU" & Tiempo == "Final", 
+                         select =-c(Tiempo, Sexo, Endulzante))
+  
+  tabla_FxSUx0 <- subset(tabla, Sexo == "MUJER" & Endulzante =="SU" & Tiempo == "0",
+                         select =-c(Tiempo, Sexo, Endulzante))
+  tabla_FxSUxF <- subset(tabla, Sexo == "MUJER" & Endulzante =="SU" & Tiempo == "Final",
+                         select =-c(Tiempo, Sexo, Endulzante))
+  
+  
+  pca_MxSAx0 <- PCA(tabla_MxSAx0, scale.unit=F)
+  print(fviz_contrib(pca_MxSAx0, choice="var", title = paste("ContribVar dim1 Sexo M Endulzante SA Tiempo 0", 
+                                                           nombreTabla)))
+  
+  print(fviz_contrib(pca_MxSAx0, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo M Endulzante SA Tiempo 0", 
+                                                                    nombreTabla)))
+  
+  pca_MxSAxF <- PCA(tabla_MxSAxF, scale.unit=F)
+  print(fviz_contrib(pca_MxSAxF, choice="var", title = paste("ContribVar dim1 Sexo M Endulzante SA Tiempo F", 
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_MxSAxF, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo M Endulzante SA Tiempo F", 
+                                                                       nombreTabla)))
+  
+  pca_FxSAx0 <- PCA(tabla_FxSAx0, scale.unit=F)
+  print(fviz_contrib(pca_FxSAx0, choice="var", title = paste("ContribVar dim1 Sexo F Endulzante SA Tiempo 0",
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_FxSAx0, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo F Endulzante SA Tiempo 0", 
+                                                                     nombreTabla)))
+                                                                     
+  pca_FxSAxF <- PCA(tabla_FxSAxF, scale.unit=F)
+  
+  print(fviz_contrib(pca_FxSAxF, choice="var", title = paste("ContribVar dim1 Sexo F Endulzante SA Tiempo F",
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_FxSAxF, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo F Endulzante SA Tiempo F",
+                                                                       nombreTabla)))
+  pca_MxSTx0 <- PCA(tabla_MxSTx0, scale.unit=F)
+  print(fviz_contrib(pca_MxSTx0, choice="var", title = paste("ContribVar dim1 Sexo M Endulzante ST Tiempo 0", 
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_MxSTx0, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo M Endulzante ST Tiempo 0", 
+                                                                     nombreTabla)))
+  pca_MxSTxF <- PCA(tabla_MxSTxF, scale.unit=F)
+  print(fviz_contrib(pca_MxSTxF, choice="var", title = paste("ContribVar dim1 Sexo M Endulzante ST Tiempo F",
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_MxSTxF, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo M Endulzante ST Tiempo F", 
+                                                                     nombreTabla)))
+  
+  
+  pca_FxSTx0 <- PCA(tabla_FxSTx0, scale.unit=F)
+  print(fviz_contrib(pca_FxSTx0, choice="var", title = paste("ContribVar dim1 Sexo F Endulzante ST Tiempo 0", nombreTabla)))
+  
+  print(fviz_contrib(pca_FxSTx0, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo F Endulzante ST Tiempo 0", 
+                                                                     nombreTabla)))
+  
+  
+  pca_FxSTxF <- PCA(tabla_FxSTxF, scale.unit=F)
+  print(fviz_contrib(pca_FxSTxF, choice="var", title = paste("ContribVar dim1 Sexo F Endulzante ST Tiempo F", nombreTabla)))
+  
+  print(fviz_contrib(pca_FxSTxF, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo F Endulzante ST Tiempo F", 
+                                                                       nombreTabla)))
+  
+  pca_MxSUx0 <- PCA(tabla_MxSUx0, scale.unit=F)
+  print(fviz_contrib(pca_MxSUx0, choice="var", title = paste("ContribVar dim1 Sexo M Endulzante SU Tiempo 0", 
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_MxSUx0, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo M Endulzante SU Tiempo 0", 
+                                                                     nombreTabla)))
+  
+  pca_MxSUxF <- PCA(tabla_MxSUxF, scale.unit=F)
+  print(fviz_contrib(pca_MxSUxF, choice="var", title = paste("ContribVar dim1 Sexo M Endulzante SU Tiempo F", 
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_MxSUxF, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo M Endulzante SU Tiempo F", 
+                                                                       nombreTabla)))
+  pca_FxSUx0 <- PCA(tabla_FxSUx0, scale.unit=F)
+  print(fviz_contrib(pca_FxSUx0, choice="var", title = paste("ContribVar dim1 Sexo F Endulzante SU Tiempo 0", 
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_FxSUx0, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo F Endulzante SU Tiempo 0", 
+                                                                       nombreTabla)))
+  
+  
+  pca_FxSUxF <- PCA(tabla_FxSUxF, scale.unit=F)
+  print(fviz_contrib(pca_FxSUxF, choice="var", title = paste("ContribVar dim1 Sexo F Endulzante SU Tiempo F", 
+                                                             nombreTabla)))
+  
+  print(fviz_contrib(pca_FxSUxF, choice="var", axes = 2, title = paste("ContribVar dim2 Sexo F Endulzante SU Tiempo F", 
+                                                                       nombreTabla)))  
 }
 
 # Orina Flavonoides ----
@@ -190,24 +357,73 @@ orinaFlav <- preprocessTablas("data/", "cronicoOrinaFlav_Antro.csv")
 orinaFlavRaw <- orinaFlav$tablaFactors
 orinaFlavNum <- orinaFlav$tablaNum
 
-
 orinaFlavNum_0 <- subset(orinaFlavNumTiempo, Tiempo == "0", select = -Tiempo)
 orinaFlavNum_F <- subset(orinaFlavNumTiempo, Tiempo == "Final", select = -Tiempo)
 
 
 ## PCA ----
 
-pruebaUwu <- pcaVarios(orinaFlav$tablaFactors)
+orinaFlav_Factors <- orinaFlav$tablaFactors
 
+pruebauwu2 <- subset(orinaFlav_Factors, Tiempo == "0" & Sexo == "HOMBRE", select =-c(Tiempo, Sexo, Endulzante))
 
+pcaVarios(orinaFlav_Factors, "orina flavonoides")
 
-fviz_contrib(prcomp(pruebaUwu[[5]]), choice="var", top = 13)
+fviz_contrib(PCA(pruebaUwu[[5]]), choice="var", top = 13)
 
 pca_datos_A      <- prcomp(orinaFlavNum_0)
 print(fviz_contrib(pca_datos_A, choice="var", top = 13))
 
-pca_datos_A      <- prcomp(orinaFlavNum_F)
-print(fviz_contrib(pca_datos_A, choice="var", top = 13))
+pca_datos_A <- prcomp(orinaFlavNum_F, scale = T)
+pca_datos_Z <- PCA(orinaFlavNum_F, graph = F)
+
+pca_datos_A
+pca_datos_Z$var$contrib
+  
+  
+fviz_contrib(pca_datos_A, choice="var")
+fviz_contrib(pca_datos_Z, choice="var")
+
+fviz_screeplot(pca_datos_A, addlabes = TRUE)
+
+PVE <- 100*pca_datos_A$sdev^2/sum(pca_datos_A$sdev^2)
+PVE
+
+par(mfrow = c(1,2))
+
+plot(PVE, type = "o", 
+     ylab = "PVE", 
+     xlab = "Componente principal", 
+     col = "blue")
+plot(cumsum(PVE), type = "o", 
+     ylab = "PVE acumulada", 
+     xlab = "Componente principal", 
+     col = "brown3")
+
+# Helper function 
+#::::::::::::::::::::::::::::::::::::::::
+var_coord_func <- function(loadings, comp.sdev){
+  loadings*comp.sdev
+}
+# Compute Coordinates
+#::::::::::::::::::::::::::::::::::::::::
+loadings <- pca_datos_A$rotation
+sdev <- pca_datos_A$sdev
+var.coord <- t(apply(loadings, 1, var_coord_func, sdev)) 
+head(var.coord[, 1:4])
+
+# Compute Cos2
+#::::::::::::::::::::::::::::::::::::::::
+var.cos2 <- var.coord^2
+head(var.cos2[, 1:4])
+
+# Compute contributions
+#::::::::::::::::::::::::::::::::::::::::
+comp.cos2 <- apply(var.cos2, 2, sum)
+contrib <- function(var.cos2, comp.cos2){var.cos2*100/comp.cos2}
+var.contrib <- t(apply(var.cos2,1, contrib, comp.cos2))
+head(var.contrib[, 1:4])
+
 
 ## Clustering ----
 
@@ -242,12 +458,12 @@ p1 <- ggparcoord(data = orinaFlavNum_F, columns = c(2:14), groupColumn = "cluste
                  title = "Clustering Tiempo Final")
 
 
-ggplotly(p1)
+ggplotly(p)
 
 plot(orinaFlavNum_0$cluster, orinaFlavNum_0$NS)
 plot(orinaFlavNum_F$cluster, orinaFlavNum_F$NS)
 
-### Orina Antocianos ---- 
+# Orina Antocianos ---- 
 
 orinaAnt <- preprocessTablas("data/", "cronicoOrinaAnt_Antro.csv")
 orinaAntRaw <- orinaAnt$tablaRaw
@@ -267,7 +483,7 @@ clusterVarios(orinaFlavNum_F, 4)
 
 
 
-### Plasma Antocianos ---- 
+# Plasma Antocianos ---- 
 
 orinaAnt <- preprocessTablas("data/", "cronicoPlasmaAnt_Antro.csv")
 orinaAntRaw <- orinaAnt$tablaRaw
@@ -279,17 +495,4 @@ orinaAntNum_0 <- subset(orinaAntNumTiempo, Tiempo == "0", select = -Tiempo)
 orinaAntNum_F <- subset(orinaAntNumTiempo, Tiempo == "Final", select = -Tiempo)
 
 
-## PCA ----
-
-pca_datos_A      <- prcomp(orinaAntNum_0)
-print(fviz_contrib(pca_datos_A, choice="var", top = 13))
-
-pca_datos_A      <- prcomp(orinaAntNum_F)
-print(fviz_contrib(pca_datos_A, choice="var", top = 13))
-
-checkCluster(orinaAntNum_0)
-checkCluster(orinaAntNum_F)
-
-clusterVarios(orinaFlavNum_0, 5)
-clusterVarios(orinaFlavNum_F, 4)
 
