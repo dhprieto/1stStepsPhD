b43ecf68e2b6b441ece6216a87401aef7e0b433f
mozoezomoe <dres2112@gmail.com>
2022-03-09 12:12:30 +0100

    ghi

--- pruebasLecturaNueva.R
+++ pruebasLecturaNueva.R
@@ -1,7 +1,7 @@
 source("scripts/preprocess.R")
 
-tabla1 <- reading("data/cronicoOrinaAntLimpio.csv", nasPercentageCol = 0.1, nasRow = T)
+tabla1 <- reading("data/cronicoOrinaAntLimpio.csv", nasPercentageCol = 0.3, nasRow = T)
 write.csv(tabla1, "data/prueba.csv")
 tabla1pp <- preprocessTables("data/", "prueba.csv")
 
-tabla1pp
+View(tabla1pp$tablaSinEsc)
